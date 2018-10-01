7678f4c93e5d12e6c985def14c25b22990ddf94e
widyantoro <43356973+widyantoro@users.noreply.github.com>
2018-10-01 21:16:35 +0700

    PBO3-10117113-Latihan18-Kelipatan

---
+++ PBO3-10117113-Latihan18-Kelipatan/src/pbo3/pkg10117113/latihan18/kelipatan/PBO310117113Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan18.kelipatan;
+
+/*
+ * Nama     : Eka Widyantoro
+ * NIM      : 10117113
+ * Kelas    : IF-3/PBO-3
+ * Desc     : Program untuk melihat kelipatan sebuah bilangan
+ */
+public class PBO310117113Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i;
+        i = 1;
+        while(i<=10){
+            System.out.println(3.5 * i);
+            i++;
+        }
+    }
+    
+}
